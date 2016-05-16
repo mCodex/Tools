e64d84d0817adac2f305fc459617da413ac50d5d
Mateus Andrade <mat.andrade@live.com>
2016-05-16 09:24:14 -0300

    few changes

--- RemoveDuplicateEntry.py
+++ RemoveDuplicateEntry.py
@@ -10,7 +10,7 @@ aux = defaultdict(list)
 vector =[]
 
 def readFile():
-	csvName = ".csv" #Put here the CSV name
+	csvName = "diff1.csv" #Put here the CSV name
 	with open(csvName) as f:
 		reader = csv.reader(f,delimiter=",") 
 		#reader.next()
@@ -21,8 +21,8 @@ def readFile():
 if __name__ == "__main__":
 	readFile()
 	for j in range(0, len(columns[0])):
-		vector.append(columns[2][j]) #Choose the column that you want to remove duplicate entries in my case "2"
+		vector.append(columns[0][j]) #Choose the column that you want to remove duplicate entries in my case "2"
 
-	arq = open (".txt", "wt") #Choose the file you may want to save the entries 
+	arq = open ("test.txt", "wt") #Choose the file you may want to save the entries 
 	for i in set(vector):
 		arq.writelines(i+"\n")
